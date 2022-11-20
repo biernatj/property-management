32e9fe8cd4d172c0eb24c0de442c68d53b574610
Joanna Biernat-Gerono <biernatj@interia.pl>
2022-11-20 21:44:09 +0100

    Implemented logging with logback and slf4j

--- src/main/java/com/mycompany/propertymanagement/exception/CustomExceptionHandler.java
+++ src/main/java/com/mycompany/propertymanagement/exception/CustomExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.mycompany.propertymanagement.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -13,11 +15,14 @@ import java.util.List;
 @ControllerAdvice
 public class CustomExceptionHandler {
 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<List<ErrorModel>> handleFieldValidation(MethodArgumentNotValidException argumentNotValidException) {
         List<FieldError> fieldErrorList = argumentNotValidException.getBindingResult().getFieldErrors();
         List<ErrorModel> errorModelList = new ArrayList<>();
         for(FieldError fieldError : fieldErrorList) {
+            logger.debug("Inside field validation -level-debug {} - {}", fieldError.getField(), fieldError.getDefaultMessage());
+            logger.info("Inside field validation -level-info {} - {}", fieldError.getField(), fieldError.getDefaultMessage());
             ErrorModel errorModel = new ErrorModel();
             errorModel.setCode(fieldError.getField());
             errorModel.setMessage(fieldError.getDefaultMessage());
@@ -28,7 +33,12 @@ public class CustomExceptionHandler {
 
     @ExceptionHandler(BusinessException.class)
     public ResponseEntity<List<ErrorModel>> handleBusinessException(BusinessException businessException) {
-        System.out.println("Business exception is thrown");
+        for(ErrorModel errorModel : businessException.getErrors()) {
+            logger.debug("Business exception is thrown -level-debug: {} - {}", errorModel.getCode(), errorModel.getMessage());
+            logger.info("Business exception is thrown -level-info: {} - {}", errorModel.getCode(), errorModel.getMessage());
+            logger.warn("Business exception is thrown -level-warn: {} - {}", errorModel.getCode(), errorModel.getMessage());
+            logger.error("Business exception is thrown -level-error: {} - {}", errorModel.getCode(), errorModel.getMessage());
+        }
         return new ResponseEntity<List<ErrorModel>>(businessException.getErrors(), HttpStatus.BAD_REQUEST);
     }
 }
